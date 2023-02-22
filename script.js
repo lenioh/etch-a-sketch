15035520f29ac7e43bd734a0595517e04a97402b
Krisz <121938751+lenioh@users.noreply.github.com>
2023-02-22 21:46:00 +0100

    got the color picker working finally. made a slider to change the canvas resolution from 16x16 up to 64x64

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-
 const container = document.getElementById("container");
-const divs = document.querySelectorAll('.grid-item');
+const slider = document.getElementById("slider");
+const valueDisplay = document.querySelectorAll("#slider-value");
+
+const colorPicker = document.getElementById("color-picker");
+
 /* Function to make 16*16 grid */
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    container.innerHTML = ''; // clear the grid before creating new items
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+      container.appendChild(cell).className = "grid-item";
+    };
   };
-};
+makeRows(slider.value, slider.value);
 
-makeRows(64, 64);
-
-
-/* 
-Hover element
-making a new class when the cursor hover over a grid item
-*/
-
-divs.forEach(div => {
-  div.addEventListener('mouseover', function() {
-    div.setAttribute('id', 'hovered');
-  });
+/* Add event listener to the container element */
+container.addEventListener('mouseover', function(event) {
+  if (event.target.className === 'grid-item') {
+    event.target.style.backgroundColor = colorPicker.value;
+    event.target.id = 'hovered';
+  }
 });
+/* Color picker */
+/* colorPicker.addEventListener('input', function(event) {
+    const colorValue = event.target.value;
+    const hovered = document.getElementById('hovered');
+    if (hovered) {
+      hovered.style.backgroundColor = colorValue;
+    }
+}); */
+
+/* Slider event listener */
+slider.addEventListener("input", (event) => {
+    const value = event.target.value;
+    valueDisplay.forEach(span => {
+      span.textContent = value;
+    });
+    makeRows(value, value);
+  });
